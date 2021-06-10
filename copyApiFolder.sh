35f4f6eea0e2c85423ce6e5a041affe682bcd2a2
Srinidhi-SA <Srinidhi.Anantharamu@marlabs.com>
2021-06-10 23:34:09 +0530

    Test push

---
+++ copyApiFolder.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+cd API_CODE/mAdvisorStgAPIUI/API_DOCKER
+docker build -t $REPOSITORY_URI_API:latest .
+docker tag $REPOSITORY_URI_API:latest $REPOSITORY_URI_API:$IMAGE_TAG_API
+$(aws ecr get-login --region us-west-1 --no-include-email) 
+docker push $REPOSITORY_URI_API:$IMAGE_TAG_API
+cd ../../../
