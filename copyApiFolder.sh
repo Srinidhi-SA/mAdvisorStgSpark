96f08ecc74c2d5727cd50294abee09826e3c71f8
Srinidhi-SA <Srinidhi.Anantharamu@marlabs.com>
2021-06-10 23:57:47 +0530

    Test push

--- copyApiFolder.sh
+++ copyApiFolder.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-cd API_CODE/mAdvisorStgAPIUI/API_DOCKER
+cd API-CODE/mAdvisorStgAPIUI/API_DOCKER
 docker build -t $REPOSITORY_URI_API:latest .
 docker tag $REPOSITORY_URI_API:latest $REPOSITORY_URI_API:$IMAGE_TAG_API
 $(aws ecr get-login --region us-west-1 --no-include-email) 
--- copyNginxFolder.sh
+++ copyNginxFolder.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-cd API_CODE/mAdvisorStgAPIUI/NGINX_DOCKER
+cd API-CODE/mAdvisorStgAPIUI/NGINX_DOCKER
 docker build -t $REPOSITORY_URI_NGINX:latest .
 docker tag $REPOSITORY_URI_NGINX:latest $REPOSITORY_URI_NGINX:$IMAGE_TAG_NGINX
 $(aws ecr get-login --region us-west-1 --no-include-email) 
